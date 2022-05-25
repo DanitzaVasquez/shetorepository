644d03f3e4163470fab068dbff56e0d84223b9e7
Danitza Vasquez Salas <danitzavazquez991@gmail.com>
2022-05-25 07:56:53 -0700

    cambios

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calcular/{edad}', function ($edad) {
+if ($edad > 0 && $edad < 18 ){
+    return 'eres menor de edad ';
+}elseif ($edad >= 18 && $edad <=100 ) {
+    return 'eres mayor de edad';
+}else {
+    return 'edad erronea';
+}
+});
+
+Route::get('/saludo/{nombre}', function ($nombre) {
+    return 'hola mi nombre es ' . $nombre;
+});
+
+
+Route::post('/saludo', function () {
+    return 'se creo un dato';
+});
